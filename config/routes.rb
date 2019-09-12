cca56ba139f1684c1fca4c0c33787aba278fee55
josephlbaker <joelucasbaker@gmail.com>
2019-09-11 18:33:02 -0700

    Added custom action for updating blog status

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  resources :blogs
   root to: 'pages#home'
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  resources :portfolios, except: [:show]
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 end
