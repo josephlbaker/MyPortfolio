92c38df63cdf6388f5f311ac9f8a4583e9d77bf7
josephlbaker <joelucasbaker@gmail.com>
2019-09-16 15:01:28 -0700

    Added navbar and default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,15 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "Joseph Baker | Blog"
+    @seo_keywords = "Joseph Baker blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = @blog.title
+    @seo_keywords = @blog.title + ", " + @blog.body
   end
 
   # GET /blogs/new
